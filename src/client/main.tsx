47f10ce0c2b7d049c723016a63a21c7c9ff654d1
hgjfkdls <fcabedov@gmail.com>
2026-04-05 17:37:49 -0400

    ADD: tailwind css

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import './net/socket';
+import './style.css';
 
 function App() {
     return (
         <div style={{ padding: 16 }}>
-            <h1>mgame</h1>
+            <h1>Shadow Tactics</h1>
             <p>Tick: {80} ms</p>
         </div>
     );
